04980972d09a02a8db7b85d96b89c919fbe20f2a
iamzivar <zivarpargati1380@gmail.com>
2026-07-10 19:40:39 +0330

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,32 +1,14 @@
 <?php
-$host = "localhost";
-$dbname = "english_language_learning";
-$username = "root";
-$password = "";
+$host = "sql208.infinityfree.com";
+$dbname = "if0_42380325_english_db";
+$username = "if0_42380325";
+$password = "glvx5qhd83fk";
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
     die("خطا در اتصال به دیتابیس: " . $e->getMessage());
 }
 ?>
-
-<!-- <?php
-$dsn = 'mysql:host=localhost;dbname=english_language_learning;charset=utf8mb4';
-$username = 'root';
-$password = '';
-$options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
-];
-try {
-    $conn = new PDO($dsn, $username, $password, $options);
-} catch (PDOException $e) {
-    die('Connection failed: ' . $e->getMessage());
-}
-?> -->
-
-
-
